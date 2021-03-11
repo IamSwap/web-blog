36296fd95ab5d0d26084b645802d4a709c3d136a
Swapnil Bhavsar <hey@swapnil.dev>
2021-03-11 21:31:49 +0530

    ✨ Add no posts component

---
+++ resources/js/Components/NoPosts.vue
@@ -0,0 +1,8 @@
+<template>
+    <div
+        class="flex flex-col justify-center items-center text-center space-y-4 p-12"
+    >
+        <img src="/images/no-posts.svg" alt="There are no posts yet" />
+        <span class="font-semibold text-xl">There are no posts yet!</span>
+    </div>
+</template>
