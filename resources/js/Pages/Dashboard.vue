dcf71198acf443d0768490883018e4010f4b9671
Swapnil Bhavsar <hey@swapnil.dev>
2021-03-11 21:33:51 +0530

    ⚡ Improve dashboard page to show posts list

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,15 +1,29 @@
 <template>
     <app-layout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
+            <div class="flex justify-between items-center">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    My Posts
+                </h2>
+
+                <inertia-link
+                    :href="route('posts.create')"
+                    class="bg-indigo-500 text-white rounded px-8 py-2"
+                >
+                    Create Post
+                </inertia-link>
+            </div>
         </template>
 
-        <div class="py-12">
+        <div class="py-8">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+                    <div v-if="posts.data.length">
+                        <posts-list :posts="posts" />
+                    </div>
+                    <div v-else>
+                        <no-posts></no-posts>
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,13 +31,18 @@
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
+import AppLayout from "@/Layouts/AppLayout";
+import PostsList from "@/Components/PostsList.vue";
+import NoPosts from "@/Components/NoPosts.vue";
 
-    export default {
-        components: {
-            AppLayout,
-            Welcome,
-        },
-    }
+export default {
+    props: {
+        posts: Object,
+    },
+    components: {
+        AppLayout,
+        PostsList,
+        NoPosts,
+    },
+};
 </script>
