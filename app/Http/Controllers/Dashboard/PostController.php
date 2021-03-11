c98fa6121b4f9962d0115271b97ea6fee387351e
Swapnil Bhavsar <hey@swapnil.dev>
2021-03-11 21:28:01 +0530

    ✨ Add PostController for managing posts in dashboard

---
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Models\Post;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Dashboard', [
+            'posts' => Post::orderBy('publication_date', 'desc')->simplePaginate()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Posts/CreatePost');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        $request->user()->posts()->create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'publication_date' => now()
+        ]);
+
+        Cache::tags('posts')->flush();
+
+        return redirect('dashboard');
+    }
+}
